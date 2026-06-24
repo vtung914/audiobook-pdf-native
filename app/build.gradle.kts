c2d7f2a9787daf62ab659885e72f3b85945e66cb
vtung914 <vtung914@gmail.com>
2026-06-24 07:39:23 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,35 +7,23 @@ plugins {
 android {
     namespace = "com.vtung.audiosach"
     compileSdk = 34
-
     defaultConfig {
         applicationId = "com.vtung.audiosach"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    kotlinOptions { jvmTarget = "17" }
 }
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("com.tom-roush:pdfbox-android:2.0.27.0")
-    
-    // Room Database
-    val room_version = "2.6.1"
-    implementation("androidx.room:room-runtime:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
-    kapt("androidx.room:room-compiler:$room_version")
-    
+    implementation("nl.siegmann.epublib:epublib-core:3.1")
+    implementation("org.jsoup:jsoup:1.16.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
