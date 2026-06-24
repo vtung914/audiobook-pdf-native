6f188a4f6d908ee87cf9cc401133e21295f926d1
vtung914 <vtung914@gmail.com>
2026-06-24 07:08:58 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt") // Cần thiết cho Room Database
 }
 
 android {
     namespace = "com.vtung.audiosach"
     compileSdk = 34
-
     defaultConfig {
         applicationId = "com.vtung.audiosach"
         minSdk = 26
@@ -14,19 +14,18 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 }
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("com.tom-roush:pdfbox-android:2.0.27.0")
+    
+    // Room Database
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
